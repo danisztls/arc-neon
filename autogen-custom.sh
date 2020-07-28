e44292b69168adaacbed6c5c66bd1e335c4955bf
Lobo <lobocinza@protonmail.com>
2020-07-28 01:14:09 -0300

    Helper script to build only gtk + gnome-shell

---
+++ autogen-custom.sh
@@ -0,0 +1,2 @@
+./autogen.sh --prefix=$HOME/.local --disable-cinnamon --disable-metacity --disable-openbox --disable-plank --disable-unity --disable-xfwm --disable-light --disable-lighter --disable-darker
+
