7fe7631feb3bd687ca61630191f130946bb46f89
Lobo <lobocinza@protonmail.com>
2020-08-01 19:35:46 -0300

    Removed references for light, lighter and darker

--- autogen-custom.sh
+++
@@ -1,2 +0,0 @@
-./autogen.sh --prefix=$HOME/.local --disable-cinnamon --disable-metacity --disable-openbox --disable-plank --disable-unity --disable-xfwm --disable-light --disable-lighter --disable-darker
-
